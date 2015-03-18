50dc3fcd3a5e5de0302d3274d2bff809c950ceac
Janek Priimann <janek.priimann@gmail.com>
2015-03-19 00:37:39 +0200

    Add WebImage class

--- app/src/main/java/ee/ajapaik/sorter/util/WebImage.java
+++ app/src/main/java/ee/ajapaik/sorter/util/WebImage.java
@@ -1,4 +1,20 @@
 package ee.ajapaik.sorter.util;
 
+import android.content.Context;
+
+import java.io.InputStream;
+
 public class WebImage extends WebOperation {
+    public WebImage(Context context, String path) {
+        super(context, path, null);
+    }
+
+    @Override
+    protected void onFailure() {
+    }
+
+    @Override
+    protected void onResponse(int statusCode, InputStream stream) {
+
+    }
 }
