9170973774422827fcec144df14d4be8fdb221e6
Janek Priimann <janek.priimann@gmail.com>
2015-03-19 19:06:26 +0200

    Update AlbumsActivity

--- app/src/main/java/ee/ajapaik/sorter/AlbumsActivity.java
+++ app/src/main/java/ee/ajapaik/sorter/AlbumsActivity.java
@@ -1,84 +1,44 @@
 package ee.ajapaik.sorter;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import ee.ajapaik.sorter.data.Feed;
-import ee.ajapaik.sorter.data.util.Status;
-import ee.ajapaik.sorter.util.WebAction;
+import ee.ajapaik.sorter.fragment.AlbumsFragment;
+import ee.ajapaik.sorter.util.WebActivity;
 
-public class AlbumsActivity extends ActionBarActivity {
+public class AlbumsActivity extends WebActivity {
     private static final String TAG = "AlbumsActivity";
-
-    private WebService.Connection m_connection = new WebService.Connection();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_albums);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new AlbumsFragment()).commit();
         }
-
-        m_connection.enqueue(this, Feed.createAction(this), new WebAction.ResultHandler<Feed>() {
-            @Override
-            public void onActionResult(Status status, Feed data) {
-                Log.e(TAG, "Feed completed: " + status.getCode());
-            }
-        });
-    }
-
-    protected void onDestroy() {
-        m_connection.dequeueAll(this);
-
-        super.onDestroy();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_albums, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_albums, container, false);
-            return rootView;
-        }
     }
 }
