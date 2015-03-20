3fd0887bc647f172f4f9e218a905395df4c98b72
Janek Priimann <janek.priimann@gmail.com>
2015-03-20 23:17:24 +0200

    Update Photo.Tag model

--- app/src/main/java/ee/ajapaik/sorter/data/Photo.java
+++ app/src/main/java/ee/ajapaik/sorter/data/Photo.java
@@ -134,15 +134,15 @@ public class Photo extends Model {
     }
 
     public enum Tag {
-        INTERIOR_OR_EXTERIOR("interior_or_exterior", R.string.photo_tag_interior, R.drawable.ic_local_hotel_white_48dp, R.string.photo_tag_exterior, R.drawable.ic_nature_people_white_48dp),
-        PUBLIC_OR_PRIVATE("public_or_private", R.string.photo_tag_public, R.drawable.ic_public_white_48dp, R.string.photo_tag_private, R.drawable.ic_vpn_lock_white_48dp),
-        URBAN_OR_RURAL("urban_or_rural", R.string.photo_tag_urban, R.drawable.ic_location_city_white_48dp, R.string.photo_tag_rural, R.drawable.ic_nature_white_48dp),
-        GROUND_OR_RAISED("ground_or_raised", R.string.photo_tag_ground, R.drawable.ic_nature_people_white_48dp, R.string.photo_tag_raised, R.drawable.ic_filter_drama_white_48dp),
-        VIEW_OR_SOCIAL("view_or_social", R.string.photo_tag_view, R.drawable.ic_landscape_white_48dp, R.string.photo_tag_social, R.drawable.ic_accessibility_white_48dp),
-        STAGED_OR_NATURAL("staged_or_natural", R.string.photo_tag_staged, R.drawable.ic_account_box_white_48dp, R.string.photo_tag_natural, R.drawable.ic_directions_walk_white_48dp),
-        ONE_OR_MANY("one_or_many", R.string.photo_tag_one, R.drawable.ic_person_white_48dp, R.string.photo_tag_many, R.drawable.ic_group_add_white_48dp),
-        WHOLE_OR_DETAIL("whole_or_detail", R.string.photo_tag_whole, R.drawable.ic_location_city_white_48dp, R.string.photo_tag_detail, R.drawable.ic_local_florist_white_48dp),
-        UNKNOWN(null, 0, 0, 0, 0);
+        INTERIOR_OR_EXTERIOR("interior_or_exterior", 0x01, R.string.photo_tag_interior, R.drawable.ic_local_hotel_white_48dp, R.string.photo_tag_exterior, R.drawable.ic_nature_people_white_48dp),
+        PUBLIC_OR_PRIVATE("public_or_private", 0x02, R.string.photo_tag_public, R.drawable.ic_public_white_48dp, R.string.photo_tag_private, R.drawable.ic_vpn_lock_white_48dp),
+        URBAN_OR_RURAL("urban_or_rural", 0x04, R.string.photo_tag_urban, R.drawable.ic_location_city_white_48dp, R.string.photo_tag_rural, R.drawable.ic_nature_white_48dp),
+        GROUND_OR_RAISED("ground_or_raised", 0x08, R.string.photo_tag_ground, R.drawable.ic_nature_people_white_48dp, R.string.photo_tag_raised, R.drawable.ic_filter_drama_white_48dp),
+        VIEW_OR_SOCIAL("view_or_social", 0x10, R.string.photo_tag_view, R.drawable.ic_landscape_white_48dp, R.string.photo_tag_social, R.drawable.ic_accessibility_white_48dp),
+        STAGED_OR_NATURAL("staged_or_natural", 0x20, R.string.photo_tag_staged, R.drawable.ic_account_box_white_48dp, R.string.photo_tag_natural, R.drawable.ic_directions_walk_white_48dp),
+        ONE_OR_MANY("one_or_many", 0x40, R.string.photo_tag_one, R.drawable.ic_person_white_48dp, R.string.photo_tag_many, R.drawable.ic_group_add_white_48dp),
+        WHOLE_OR_DETAIL("whole_or_detail", 0x80, R.string.photo_tag_whole, R.drawable.ic_location_city_white_48dp, R.string.photo_tag_detail, R.drawable.ic_local_florist_white_48dp),
+        UNKNOWN(null, R.string.photo_tag_na, 0x00, R.drawable.ic_cancel_white_48dp, R.string.photo_tag_na, R.drawable.ic_cancel_white_48dp);
 
         public static Tag parse(String code) {
             if(code != null) {
@@ -157,17 +157,23 @@ public class Photo extends Model {
         }
 
         private final String m_code;
+        private final int m_mask;
         private final int m_leftTitleResourceId;
         private final int m_leftImageResourceId;
         private final int m_rightTitleResourceId;
         private final int m_rightImageResourceId;
 
-        private Tag(final String code, final int leftTitleResourceId, final int leftImageResourceId, final int rightTitleResourceId, final int rightImageResourceId) {
+        private Tag(final String code, final int mask, final int leftTitleResourceId, final int leftImageResourceId, final int rightTitleResourceId, final int rightImageResourceId) {
             m_code = code;
+            m_mask = mask;
             m_leftTitleResourceId = leftTitleResourceId;
             m_leftImageResourceId = leftImageResourceId;
             m_rightTitleResourceId = rightTitleResourceId;
             m_rightImageResourceId = rightImageResourceId;
+        }
+
+        public int getMask() {
+            return m_mask;
         }
 
         public int getLeftTitleResourceId() {
